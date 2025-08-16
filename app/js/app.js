6cc58a5f8b713af48b8f1cd0caf6cf8dd2fc0573
wizzard1010 <thutaag2810@gmail.com>
2025-08-17 00:06:27 +0700

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+function add(){
+    return a + b; 
+}
+
+function sub(a, b){ 
+    if (a < b) {
+        return b - a;
+    } else {
+        return a - b;
+    }   
+}
+
+function show() {
+    //
+}
+
+function display() {
+    //
+}
